e4f94ec2f6b9533640f8791a28edec00747bd66c
William Yang <mscame@gmail.com>
2023-02-13 18:04:03 +0100

    chore(TLS):  hibernate_after default to 5s, follow version 4.x

--- apps/emqx/src/emqx_schema.erl
+++ apps/emqx/src/emqx_schema.erl
@@ -1963,8 +1963,7 @@ common_ssl_opts_schema(Defaults) ->
             sc(
                 duration(),
                 #{
-                    default => undefined,
-                    required => false,
+                    default => Df("hibernate_after", "5s"),
                     desc => ?DESC(common_ssl_opts_schema_hibernate_after)
                 }
             )}
